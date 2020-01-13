085936f6e2aa19c1d5c65863c476160abc75735a
Harrison Levin <harrison483@gmail.com>
2020-01-13 09:19:21 -0700

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_160555) do
+ActiveRecord::Schema.define(version: 2020_01_13_161754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_01_13_160555) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "flight_passengers", force: :cascade do |t|
+    t.bigint "flight_id"
+    t.bigint "passenger_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_flight_passengers_on_flight_id"
+    t.index ["passenger_id"], name: "index_flight_passengers_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -38,5 +47,7 @@ ActiveRecord::Schema.define(version: 2020_01_13_160555) do
     t.integer "age"
   end
 
+  add_foreign_key "flight_passengers", "flights"
+  add_foreign_key "flight_passengers", "passengers"
   add_foreign_key "flights", "airlines"
 end
