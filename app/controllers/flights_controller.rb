817ef5df8a31b6fbcef4e78d38972e83ad747258
Harrison Levin <harrison483@gmail.com>
2020-01-13 10:21:02 -0700

    add flight to passenger

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,4 +4,11 @@ class FlightsController <ApplicationController
     @flight = Flight.find(params[:id])
   end
 
+  def create
+    passenger = Passenger.find(params[:passenger_id])
+    flight = Flight.look_up_flight(params[:number])
+    passenger.flights << flight
+    redirect_to passenger_path(passenger)
+  end
+
 end
