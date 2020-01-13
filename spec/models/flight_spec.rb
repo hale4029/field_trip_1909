ecf56f7a28d7f017045e14fd2351309a59811eeb
Harrison Levin <harrison483@gmail.com>
2020-01-13 10:28:30 -0700

    look_up model test

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Flight, type: :model do
     it {should have_many :flight_passengers}
     it {should have_many(:passengers).through(:flight_passengers)}
   end
+
+  describe "methods" do
+    it "look_up_flight by flight :number" do
+      southwest = Airline.create(name: "Southwest")
+      southwest_1 = southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+
+      result = Flight.look_up_flight(southwest_1.number)
+      expect(result).to eq(southwest_1)
+    end
+  end
+
+
 end
