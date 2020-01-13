4d51134b289d0b2b1f7856eb4ad2ac462c5c8889
Harrison Levin <harrison483@gmail.com>
2020-01-13 10:48:36 -0700

    flight model tests

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -16,12 +16,26 @@ RSpec.describe Flight, type: :model do
   end
 
   describe "model methods" do
-    it "look_up_flight by flight :number" do
+    before :each do
       southwest = Airline.create(name: "Southwest")
-      southwest_1 = southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+      @southwest_1 = southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+      @passenger_1 = @southwest_1.passengers.create(name: "Harrison", age: 27)
+      @passenger_2 = @southwest_1.passengers.create(name: "Ellyn", age: 10)
+      @passenger_3 = @southwest_1.passengers.create(name: "Tom", age: 60)
+    end
+    it "look_up_flight by flight :number" do
+      result = Flight.look_up_flight(@southwest_1.number)
+      expect(result).to eq(@southwest_1)
+    end
 
-      result = Flight.look_up_flight(southwest_1.number)
-      expect(result).to eq(southwest_1)
+    it "count adults on flight" do
+      result = @southwest_1.number_of_adults
+      expect(result).to eq(2)
+    end
+
+    it "count minors on flight" do
+      result = @southwest_1.number_of_minors
+      expect(result).to eq(1)
     end
   end
 
